0266182c9be2b39b60a615512733a5d22a710453
semieZX <zx18846031359@aliyun.com>
2018-05-21 20:26:26 +0800

    my own q347.py

--- q347.py
+++ q347.py
@@ -1,3 +1,4 @@
+import heapq
 class Solution:
     def topKFrequent(self, nums, k):
         """
@@ -5,5 +6,14 @@ class Solution:
         :type k: int
         :rtype: List[int]
         """
-        num_count = collections.Counter(nums)
-        return heapq.nlargest(k,num_count,key = lambda x: num_count[x])
+        res = []
+        d = collections.Counter(nums)
+        list_d = [[va,key] for key,va in d.items()]
+        n = len(list_d)
+        heapq.heapify(list_d)
+        for i in range(n-k):
+            heapq.heappop(list_d)
+        for i in range(k):
+            res.append(heapq.heappop(list_d)[1])
+        return res
+        
